97a934166c8073f35fdef21c6246e82fd80bc018
jdsmith575 <39533191+jdsmith575@users.noreply.github.com>
2026-02-16 11:21:29 -0600

    Update upload_image.sh

--- upload_image.sh
+++ upload_image.sh
@@ -4,8 +4,9 @@
 # set up variables
 TV_IP="192.168.1.204"     # IP address of your TV
 OUTPUT_DIR="/home/my-user/disneyland-countdown/img/output"
+PROJECT_ROOT="/home/my-user/samsung-tv-ws-api"
 
 # activate the python virtual environment
-source /home/fd82937/samsung-tv-ws-api/.venv/bin/activate
+source ${PROJECT_ROOT}/.venv/bin/activate
 
-python /home/my-user/samsung-tv-ws-api/example/async_art_update_from_directory.py ${TV_IP} --folder ${OUTPUT_DIR} --check 0
+python ${PROJECT_ROOT}/example/async_art_update_from_directory.py ${TV_IP} --folder ${OUTPUT_DIR} --check 0
