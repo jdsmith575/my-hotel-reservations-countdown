b93ce2983d3a6111296d0ac9cdd3e4c70f09560b
jdsmith575 <jdsmith575@gmail.com>
2026-05-12 14:13:37 -0500

    add function to pick the border color for less hardcoding in the main script

--- common_functions.sh
+++ common_functions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -x
 
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
@@ -6,4 +7,22 @@ err() {
 
 log() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
+}
+
+pick_border_color() {
+  case "${1}" in
+    "contemporary")
+      BORDER_COLOR="#5294DC"
+      ;;
+    "grand_californian")
+      BORDER_COLOR="#FAC04B"
+      ;;
+    "pop_century")
+      BORDER_COLOR="#498BD9"
+      ;;
+    *)
+      err "Could not find hotel! Exiting."
+      exit 1
+  esac
+  echo "${BORDER_COLOR}"
 }
